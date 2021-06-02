9cb92d91820eaf95d18f73f4ea5f17f23bbeb6a0
utkarshmalkoti <61866508+utkarshmalkoti@users.noreply.github.com>
2021-06-03 03:10:23 +0530

    Update VA.py

--- VA.py
+++ VA.py
@@ -13,14 +13,11 @@ def create_dataset():
     va_parts = os.listdir(va_path)
     va_parts.remove('dataset_va_4.json')
 
-    # x = []
     dataset={'img_arrays':[],'valence' : [], 'arousal' : []}
 
-    # breakpoint()
     for part in va_parts:
         print(part)
         all_videos = os.listdir(os.path.join(va_path,part))
-        # breakpoint()
         if os.path.exists(os.path.join(va_path,part,'README.md')):
             all_videos.remove('README.md')
         for video in all_videos:
@@ -30,14 +27,11 @@ def create_dataset():
                 f = open(os.path.join("valence and arousal",part,video,video+'.json'),'r')
                 data_va = json.load(f) 
                 f.close()
-                # frames = list(data_va["frames"].keys())
-                # breakpoint()                        #valence and arousal\01\001\00000.png
                 gray_img = cv2.imread(os.path.join(va_path,part,video,frame),cv2.IMREAD_GRAYSCALE)
                 gray_img_resized = cv2.resize(gray_img,(64,64))
                 dataset['img_arrays'].append(gray_img_resized.tolist())
                 dataset['valence'].append(data_va['frames'][frame.split('.')[0]]['valence'])
                 dataset['arousal'].append(data_va['frames'][frame.split('.')[0]]['arousal'])
-    breakpoint()
     f = open("valence and arousal/dataset_va_all.json",'w')
     json.dump(dataset,f)
 
@@ -51,23 +45,14 @@ def load_dataset():
     x_test = np.asarray(x_test).reshape(-1,64,64,1)/255
     y_train = np.asarray(y_train)
     y_test = np.asarray(y_test)
-    breakpoint()
     return x_train, x_test, y_train, y_test
-load_dataset()
 
 def neural_net():
-
     x_train, x_test, y_train, y_test = load_dataset()
     inp = Input(shape = (x_train[0].shape))
     conv1 = Conv2D(64,(4,4),2,activation='relu')(inp)
     batchnorm1 = BatchNormalization()(conv1)
     max_pool1 = MaxPool2D((2,2),padding='same')(batchnorm1)
-    # conv2 = Conv2D(64,(4,4),2,activation='relu')(max_pool1)
-    # batchnorm2 = BatchNormalization()(conv2)
-    # # max_pool2 = MaxPool2D((2,2),padding='same')(batchnorm2)
-    # conv3 = Conv2D(64,(4,4),2,activation='relu')(batchnorm2)
-    # batchnorm3 = BatchNormalization()(conv3)
-    # max_pool2 = MaxPool2D((2,2),padding='same')(batchnorm3)
     drop1= Dropout(0.3)(max_pool1)
     flat = Flatten()(drop1)
     dense = Dense(32,activation='relu')(flat)
@@ -82,6 +67,7 @@ def neural_net():
             return lr
     lr = tensorflow.keras.callbacks.LearningRateScheduler(scheduler)
     adam = Adam(learning_rate=0.001)
+    earlystop = tensorflow.keras.callbacks.EarlyStopping(monitor='val_loss',min_delta=0.0001,patience=5,mode='min',restore_best_weights=True,verbose=1)
     chkpoint = tensorflow.keras.callbacks.ModelCheckpoint("va model/weights2.h5",monitor='loss',save_best_only=True,mode='min')
     if os.path.exists("va model/weights2.h5"):
         model.load_weights("va model/weights2.h5")
@@ -92,8 +78,7 @@ def neural_net():
         f.write(json_model)
         print("model_saved")
     model.compile(loss={'Valence':'mae','Arousal':'mae'}, metrics=['accuracy'],optimizer=adam)
-    model.fit(x_train,y_train,batch_size=32,epochs=100,validation_data=(x_test,y_test),callbacks=[chkpoint,lr])
-# neural_net()
-# create_dataset()
+    model.fit(x_train,y_train,batch_size=32,epochs=100,validation_data=(x_test,y_test),callbacks=[chkpoint,lr,earlystop])
+neural_net()
 
 
